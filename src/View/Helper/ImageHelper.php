f42ab7430ada249549daff9e3135c311b4354e63
Kaliel <maxime@kaliel.fr>
2019-01-10 17:27:04 +0100

    cc

--- src/View/Helper/ImageHelper.php
+++ src/View/Helper/ImageHelper.php
@@ -20,8 +20,13 @@ class ImageHelper extends Helper
 
     public $helpers = ['Html'];
 
-    public function resize($url)
+    public function resize($url, $params, $options)
     {
-        return $this->Html->Image($url);
+        return $this->Html->Image($url, $params, $options);
+    }
+
+    public function resizeUrl($url)
+    {
+
     }
 }
