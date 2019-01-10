6d6c7c0152503a4954467179e66decc943a37fa8
Kaliel <maxime@kaliel.fr>
2019-01-10 18:34:38 +0100

    add intervention

--- src/View/Helper/ImageHelper.php
+++ src/View/Helper/ImageHelper.php
@@ -2,8 +2,9 @@
 
 namespace Image\View\Helper;
 
+use Cake\Filesystem\File;
 use Cake\View\Helper;
-use Cake\View\View;
+use Intervention\Image\ImageManager;
 
 /**
  * Image helper
@@ -16,17 +17,40 @@ class ImageHelper extends Helper
      *
      * @var array
      */
-    protected $_defaultConfig = [];
+    protected $_defaultConfig = [
+        'afterFilename' => '_',
+        'afterWidth' => 'x',
+        'afterHeight' => '_'
+    ];
 
     public $helpers = ['Html'];
 
-    public function resize($url, $params, $options)
+    public function resize($url, $width = null, $height = null, $quality = 60, $optimize = true, array $options = [])
     {
-        return $this->Html->Image($this->resizeUrl($url), $params, $options);
+        return $this->Html->Image($this->resizeUrl($url, $width, $height, $quality, $optimize), $options);
     }
 
-    public function resizeUrl($url)
+    public function resizeUrl($url, $width = null, $height = null, $quality = 60, $optimize = true)
     {
-        return $url;
+
+        $imageFile = new File(WWW_ROOT . 'img' . DS . $url);
+        if (!$imageFile->exists()) {
+            return null;
+        }
+
+        $thumbFileName = $imageFile->name() . $this->getConfig('afterFilename') . $width . $this->getConfig('afterWidth') . $height . $this->getConfig('afterHeight') . $quality . '.' . strtolower($imageFile->ext());
+        $newThumbFilePath = $imageFile->Folder->path . DS . $thumbFileName;
+
+        if (!(new File($newThumbFilePath))->exists()) {
+
+            $manager = new ImageManager();
+            $manager->make($imageFile->path)
+                ->orientate()//prevent weird orientation from iphone pictures
+                ->fit($width, $height)
+                ->save($newThumbFilePath)
+                ->destroy();
+        }
+
+        return str_replace(WWW_ROOT . 'img' . DS, '', $newThumbFilePath);
     }
 }
